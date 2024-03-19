25b0b9a8d4396f6835be2630ce5b230d1e5a06e9
masa73044 <108378047+masa73044@users.noreply.github.com>
2024-03-19 08:59:53 +0700

    testing

--- app/src/server/payments/stripeUtils.ts
+++ app/src/server/payments/stripeUtils.ts
@@ -3,6 +3,7 @@ import Stripe from 'stripe';
 const stripe = new Stripe(process.env.STRIPE_KEY!, {
   apiVersion: '2022-11-15',
 });
+//test
 
 // WASP_WEB_CLIENT_URL will be set up by Wasp when deploying to production: https://wasp-lang.dev/docs/deploying
 const DOMAIN = process.env.WASP_WEB_CLIENT_URL || 'http://localhost:3000';
